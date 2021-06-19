80c46d862974362d55c54ee16f00c9a892053f4c
alisaotto <alisa.otto@hotmail.de>
2021-06-19 15:53:48 +0200

    style update

--- Seite/main.js
+++ Seite/main.js
@@ -27,12 +27,12 @@ window.onclick = function (event) {
 var slideIndex0 = 1;
 showDivs(slideIndex0);
 
-var slideIndex1= 1;
+var slideIndex1 = 1;
 showText(slideIndex1);
 
 function plusDivs(n) {
     showDivs(slideIndex0 += n);
-    showText(slideIndex1 +=n);
+    showText(slideIndex1 += n);
 }
 
 function showDivs(n) {
@@ -82,53 +82,89 @@ var slideIndex3 = 1;
 showDivs3(slideIndex3);
 
 function plusDivs3(n) {
-  showDivs3(slideIndex3 += n);
+    showDivs3(slideIndex3 += n);
 }
 
 function currentDiv(n) {
-  showDivs3(slideIndex3 = n); // Funktion, der den Bild-Slide anzeigen lässt, der angeklickt wird (CurrentDiv(1/2/3))
-  showSlides(slideIndex4 = n);
+    showDivs3(slideIndex3 = n); // Funktion, der den Bild-Slide anzeigen lässt, der angeklickt wird (CurrentDiv(1/2/3))
+    showSlides(slideIndex4 = n);
 }
 
 function showDivs3(n) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > x.length) {slideIndex3 = 1}    
-  if (n < 1) {slideIndex3 = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex3-1].style.display = "block";  
+    var i;
+    var x = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > x.length) { slideIndex3 = 1 }
+    if (n < 1) { slideIndex3 = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex3 - 1].style.display = "block";
 }
 
 var slideIndex4 = 1;
 showSlides(slideIndex4);
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("text-behind-the-beans");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex4 = 1}    
-  if (n < 1) {slideIndex4 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex4-1].style.display = "block";  
-  dots[slideIndex4-1].className += " active";
-} 
+    var i;
+    var slides = document.getElementsByClassName("text-behind-the-beans");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex4 = 1 }
+    if (n < 1) { slideIndex4 = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex4 - 1].style.display = "block";
+    dots[slideIndex4 - 1].className += " active";
+}
+
+// Fade In Effekt
+function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var result = rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= 300 + (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+    if (result && element.className == "call-to-action") {
+        console.log(element.className + " Top: " + rect.top + " Left: " + rect.left + " Bottom: " + rect.bottom + " Right: " + rect.right);
+    }
+
+    return result;
+}
+
+function callbackFunc() {
+    setVisible(document.querySelectorAll(".call-to-action"));
+    setVisible(document.querySelectorAll(".testimonial"));
+    setVisible(document.querySelectorAll(".services"));
+    setVisible(document.querySelectorAll(".behind-the-beans"));
+    setVisible(document.querySelectorAll(".div1"));
+    setVisible(document.querySelectorAll(".div2"));
+
+}
+
+function setVisible(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        if (isElementInViewport(elements[i])) {
+            elements[i].classList.add("visible");
+        }
+    }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
 
 /* $(document).on("scroll", function() {
     var pageTop = $(document).scrollTop();
     var pageBottom = pageTop + $(window).height();
     var tags = $(".call-to-action");
-  
+
     for (var i = 0; i < tags.length; i++) {
       var tag = tags[i];
-  
+
       if ($(tag).position().top < pageBottom) {
         $(tag).addClass("visible");
       } else {
@@ -136,8 +172,3 @@ function showSlides(n) {
       }
     }
   }); */
-
-
-
-
-
